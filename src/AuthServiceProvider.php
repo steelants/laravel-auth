be4fa7d41773a27f075548e1a76882c5c98a5bc1
JonatanRek <gamerclassn7@gmail.com>
2024-02-05 20:04:06 +0100

    Register Command

--- src/AuthServiceProvider.php
+++ src/AuthServiceProvider.php
@@ -6,11 +6,18 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Route;
 
 use SteelAnts\LaravelAuth\Routing\AuthRoutesMixin;
+use SteelAnts\LaravelAuth\Console\Commands\Install;
 
 class AuthServiceProvider extends ServiceProvider
 {
     public function register()
     {
+        if (!$this->app->runningInConsole())
+            return;
+
+        $this->commands([
+            Install::class
+        ]);
     }
 
     public function boot()
